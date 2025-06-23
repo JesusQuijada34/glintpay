b17785ffff176ec40b71eb12eab093b9c35f0d25
Jesus Quijada <jesusdavid34553902@gmail.com>
2025-06-23 18:28:59 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+const resultados = [
+  "✅ Transacción Exitosa",
+  "❌ Clave Incorrecta",
+  "🔒 Tarjeta Bloqueada",
+  "🪙 Fondos Insuficientes",
+  "🔌 Error de Conexión",
+  "❎ Transacción fallida",
+];
+
 document.getElementById("formulario").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Consulta realizada. ¡Gracias por usar GlintPay!");
+  const resultado = resultados[Math.floor(Math.random() * resultados.length)];
+  
+  const alerta = document.createElement("div");
+  alerta.textContent = resultado;
+  alerta.className = "alerta";
+  document.body.appendChild(alerta);
+  
+  setTimeout(() => {
+    alerta.remove();
+  }, 3000);
 });
-
-const osInfo = document.getElementById("os-info");
-let os = "Desconocido";
-
-if (navigator.userAgent.indexOf("Win") !== -1) os = "Windows";
-else if (navigator.userAgent.indexOf("Mac") !== -1) os = "MacOS";
-else if (navigator.userAgent.indexOf("Linux") !== -1) os = "Linux";
-else if (/Android/i.test(navigator.userAgent)) os = "Android";
-else if (/iPhone|iPad/i.test(navigator.userAgent)) os = "iOS";
-
-osInfo.textContent = `Sistema Detectado: ${os}`;
